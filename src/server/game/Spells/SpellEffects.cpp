8591a993cf3a7290e8e0e98ed5483ed35c5e0e42
ReZaRrChIk <rezarrchik@gmail.com>
2025-11-03 18:34:37 +0300

    Update SpellEffects.cpp
    chromie

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -365,7 +365,7 @@ NonDefaultConstructible<SpellEffectHandlerFn> SpellEffectHandlers[TOTAL_SPELL_EF
     &Spell::EffectUnused,                                   //274 SPELL_EFFECT_274
     &Spell::EffectUnused,                                   //275 SPELL_EFFECT_275
     &Spell::EffectLearnTransmogIllusion,                    //276 SPELL_EFFECT_LEARN_TRANSMOG_ILLUSION
-    &Spell::EffectNULL,                                     //277 SPELL_EFFECT_SET_CHROMIE_TIME
+    &Spell::EffectSetChromieTime,                           //277 SPELL_EFFECT_SET_CHROMIE_TIME
     &Spell::EffectNULL,                                     //278 SPELL_EFFECT_278
     &Spell::EffectNULL,                                     //279 SPELL_EFFECT_LEARN_GARR_TALENT
     &Spell::EffectUnused,                                   //280 SPELL_EFFECT_280
@@ -6305,4 +6305,24 @@ void Spell::EffectSetPlayerDataFlagCharacter()
         return;
 
     target->SetDataFlagCharacter(effectInfo->MiscValue, damage != 0);
+}
+
+void Spell::EffectSetChromieTime()
+{
+    if (effectHandleMode != SPELL_EFFECT_HANDLE_HIT_TARGET)
+        return;
+
+    Player* caster = m_caster->ToPlayer();
+    if (!caster)
+        return;
+
+    // TODO: Add/Remove ContentTuningConditionMask from ConditionalContentTuning.db2 and filter lfg for each expansion
+
+    if (!effectInfo->MiscValue)
+        caster->SetChromieTimeExpansion(0, 0); // Selected the Present
+    else
+    {
+        UiChromieTimeExpansionInfoEntry const* expansion = sUiChromieTimeExpansionInfoStore.LookupEntry(effectInfo->MiscValue);
+        caster->SetChromieTimeExpansion(expansion->ID, expansion->ExpansionLevelMask);
+    }
 }
