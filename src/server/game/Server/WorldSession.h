4981906beaff4e6d27ff66c7ac1ab962cc5a036c
ReZaRrChIk <rezarrchik@gmail.com>
2025-11-03 18:32:20 +0300

    Update WorldSession.h
    chromie

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -563,6 +563,7 @@ namespace WorldPackets
         class TrainerBuySpell;
         class RequestStabledPets;
         class SetPetSlot;
+        class ChromieTimeSelectExpansion;
     }
 
     namespace Party
@@ -1471,6 +1472,7 @@ class TC_GAME_API WorldSession
         void HandleBinderActivateOpcode(WorldPackets::NPC::Hello& packet);
         void HandleRequestStabledPets(WorldPackets::NPC::RequestStabledPets& packet);
         void HandleSetPetSlot(WorldPackets::NPC::SetPetSlot& setPetSlot);
+        void HandleChromieTimeSelectExpansionOpcode(WorldPackets::NPC::ChromieTimeSelectExpansion& selectExpansion);
 
         void HandleCanDuel(WorldPackets::Duel::CanDuel& packet);
         void HandleDuelResponseOpcode(WorldPackets::Duel::DuelResponse& duelResponse);
