8cf0190d091e4e5a22a9a9186bd6b72021655b83
ReZaRrChIk <rezarrchik@gmail.com>
2025-11-03 18:26:40 +0300

    Update NPCPackets.cpp
    chromie

--- src/server/game/Server/Packets/NPCPackets.cpp
+++ src/server/game/Server/Packets/NPCPackets.cpp
@@ -274,4 +274,10 @@ void SetPetSlot::Read()
     _worldPacket >> DestSlot;
     _worldPacket >> StableMaster;
 }
+
+void ChromieTimeSelectExpansion::Read()
+{
+    _worldPacket >> GUID;
+    _worldPacket >> Expansion;
+}
 }
