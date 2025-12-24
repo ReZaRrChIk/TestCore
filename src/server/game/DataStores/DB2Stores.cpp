837805fd1717e69cd4e2fa16f2fd727009519fab
ReZaRrChIk <rezarrchik@gmail.com>
2025-12-24 22:02:56 +0300

    Fix instantiation of sUiChromieTimeExpansionInfoStore

--- src/server/game/DataStores/DB2Stores.cpp
+++ src/server/game/DataStores/DB2Stores.cpp
@@ -386,7 +386,7 @@ DB2Storage<TransmogSetGroupEntry>               sTransmogSetGroupStore("Transmog
 DB2Storage<TransmogSetItemEntry>                sTransmogSetItemStore("TransmogSetItem.db2", &TransmogSetItemLoadInfo::Instance);
 DB2Storage<TransportAnimationEntry>             sTransportAnimationStore("TransportAnimation.db2", &TransportAnimationLoadInfo::Instance);
 DB2Storage<TransportRotationEntry>              sTransportRotationStore("TransportRotation.db2", &TransportRotationLoadInfo::Instance);
-DB2Storage<UiChromieTimeExpansionInfoEntry>     sUiChromieTimeExpansionInfoStore("UiChromieTimeExpansionInfo.db2", UiChromieTimeExpansionInfoLoadInfo::Instance());
+DB2Storage<UiChromieTimeExpansionInfoEntry>     sUiChromieTimeExpansionInfoStore("UiChromieTimeExpansionInfo.db2", &UiChromieTimeExpansionInfoLoadInfo::Instance);
 DB2Storage<UiMapEntry>                          sUiMapStore("UiMap.db2", &UiMapLoadInfo::Instance);
 DB2Storage<UiMapAssignmentEntry>                sUiMapAssignmentStore("UiMapAssignment.db2", &UiMapAssignmentLoadInfo::Instance);
 DB2Storage<UiMapLinkEntry>                      sUiMapLinkStore("UiMapLink.db2", &UiMapLinkLoadInfo::Instance);
